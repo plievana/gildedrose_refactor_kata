00f09fec99385513d0c1bae5430afdb838145b09
Pedro <pedrolp08@gmail.com>
2020-12-15 23:10:29 +0100

    Added .gitignore and separat item class in new file

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from .item import Item
 
 class GildedRose(object):
 
@@ -34,13 +35,3 @@ class GildedRose(object):
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
-
-
-class Item:
-    def __init__(self, name, sell_in, quality):
-        self.name = name
-        self.sell_in = sell_in
-        self.quality = quality
-
-    def __repr__(self):
-        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
